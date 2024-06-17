169dde5351cdee3996419ad49a61aa7b26472af5
Samanvith <Samanvith2005@gmail.com>
2024-06-17 17:05:11 +0530

    installed some dependices

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-black font-bold text-4xl text-white'>
+      Roommatch
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ9HaaKvKzqKUaTHT-pwzTeZYg3gQA6Hg",
+  authDomain: "roommatch-f613f.firebaseapp.com",
+  projectId: "roommatch-f613f",
+  storageBucket: "roommatch-f613f.appspot.com",
+  messagingSenderId: "735247007693",
+  appId: "1:735247007693:web:febc3e105c4de99fdf0e84",
+  measurementId: "G-J2X4X3L98D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
